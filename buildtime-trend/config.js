db24699a6add90065b9bf03eb68473a8046c31bd
travis-ci <travis@travis-ci.org>
2015-01-05 16:59:31 +0000

    buildtime-trend of build 22.1 synchronised with gh-pages

--- buildtime-trend/config.js
+++ buildtime-trend/config.js
@@ -1,10 +1,11 @@
 var keenConfig = {
     projectId: "53416589ce5e433082000002",
-    readKey: "f8e80b3d36d1ce3a23584d7d94bb6b107368125e248de4d7a2b1591da127d7d6222140addc5db9fbd0d0963792cffc82331e5be09d55cff01e067dfa2efabab42abf9008508cb42cd6975ae01d59e06723b7fe324d3c8fc20fbc65f96e7e76919c089c826edbea573b2dc1dccc0e01bfd5150baf797d3fa68fa7bb7e93315c99b2f4b7820bd4944d30ebe21c8555ff15237f603a80f069d99700a88220a1d0089d59db373110d35719e34136ea315130"
+    readKey: "7376710485cca43db590d989c8f7ca4aea73b6e267ff94f7a2ed50d3516f754afdb267512be5eb288b29c983d935730b7c93d01d5de018b980e453cdf1f1e6303a4d501041d8c9670a36a56b883729d29923ddd5d90b3d97f0c3ee5d731db69360de017cb9e221bca32db506de0b0461cdae6d514e6d4e1898f3413f4448a97cc9f057534adc5cc1af8a24d49ce3ee390d0abec43fdf620f4eaddcd8b07e1a005d4dff186742e1414a9fb3a8c80c24cd"
 };
 
 var config = {
     projectName: "buildtimetrend/python-client", // descriptive project name (used in the title)
     repoName: "buildtimetrend/python-client", // repo name, fe. "buildtimetrend/python-client"
-    serviceUrl: "service_url" // url to Buildtime Trend as a Service, fe. https://buildtimetrend-service.herokuapp.com/
+    serviceUrl: "service_url", // url to Buildtime Trend as a Service, fe. https://buildtimetrend-service.herokuapp.com/
+    websiteUrl: "website_url" // url to project website
 };
